7dabf616d38b527c922e2e70b3c1045ec8c82a86
Arzeres <134496974+Arzeres@users.noreply.github.com>
2024-12-18 14:33:04 +0100

    button control

---
+++ Assets/ButtonControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonControl : MonoBehaviour
+{
+    //Play Game Button
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+    //Multi Player mode button
+    public void MultiPlayer()
+    {
+        Debug.Log("Bzzzz");
+    }
+    //Quit Game button
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
